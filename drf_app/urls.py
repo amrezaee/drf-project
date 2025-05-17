443e1ea2bea4c199ff416773194876175ec55201
Amirmohamad Rezaee <amrezaee@protonmail.com>
2025-05-17 19:56:09 +0330

    add django silk profiler

--- drf_app/urls.py
+++ drf_app/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("api.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("api.urls")),
+    path("silk/", include("silk.urls", namespace="silk")),
+]
